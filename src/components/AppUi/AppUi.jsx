416a804eb6c63f497497e539c0ef01864c5cff82
Diego Dorado <diegodorado78@gmail.com>
2022-07-08 09:17:00 -0500

    antes de usar el hook  useContext

--- src/components/AppUi/AppUi.jsx
+++ src/components/AppUi/AppUi.jsx
@@ -5,58 +5,49 @@ import {TodoSearch} from '../TodoSearch/TodoSearch';
 import {TodoList} from '../TodoList/TodoList';
 import {CreateTodoButton} from '../CreateTodoButton/CreateTodoButton';
 import demonPic from '../../assets/demon.png';
-
-function AppUi({
-    loading,
-    error,
-    totalTodos,
-    completedTodos,
-    searchValue,
-    searchedTodos,
-    setSearchValue,
-    completeTodo,
-    deleteTodo,
-    todos,
-    setTodos
-}){
+import { TodoProvider } from '../TodoContext/TodoContext';
+import { TodoContext } from '../TodoContext/TodoContext';
+function AppUi(){
 
     return (
         <React.Fragment>
           <img src ={demonPic} width='250px' alt="logo"></img>
           <h1> Tanjiro's to do list </h1>
-          <TodoCounter
-           total={totalTodos}
-           completed={completedTodos}
-          />
-          
-          <TodoSearch
-          //el primer searchValue es la prop que recibe el componente el otro es la var(estado) que estoy pasando
-          searchValue={searchValue}
-          setSearchValue={setSearchValue}
-          />
-          <TodoList>
-          {/* Si la var loading es true carga el parrafo */}
-            {error && <p> Upss hubo un error con tu consulta</p>} 
-            {loading && <p> Loading...</p>} 
-            {/* si ya no esta cargando y el array esta vacio entonces */}
-            {(!loading && !searchedTodos.lenght) && <p> Create your first Todo</p>} 
+          {/* dentro de todo counter y todo search me encargo de consumir directamente el estado global */}
 
-         {searchedTodos.map(todo=> (
-          <TodoItem 
-          key={todo.text} 
-          text={todo.text}
-          completed={todo.completed}
-          onComplete={()=>completeTodo(todo.text)}
-          onDelete={()=>deleteTodo(todo.text)}
-          />
-         ))} 
-         </TodoList>
+          <TodoCounter />
+          <TodoSearch  />
+
+         <TodoContext.Consumer>
+          {/* render props=> son una funcion que reciben el obejto value que proviene del provider */}
+            {({
+            error,
+            loading,
+            searchedTodos,
+            completeTodo,
+            deleteTodo
+        }) =>(// la funcion permite recibir el estado del provider y devolver elementos paso para el return de jsx
+             <TodoList>
+               {/* Si la var loading es true carga el parrafo */}
+               {error && <p> Upss hubo un error con tu consulta</p>} 
+               {loading && <p> Loading...</p>} 
+               {/* si ya no esta cargando y el array esta vacio entonces */}
+               {(!loading && !searchedTodos.lenght) && <p> Create your first Todo</p>} 
+   
+            {searchedTodos.map(todo=> (
+             <TodoItem 
+             key={todo.text} 
+             text={todo.text}
+             completed={todo.completed}
+             onComplete={()=>completeTodo(todo.text)}
+             onDelete={()=>deleteTodo(todo.text)}
+             />
+            ))} 
+            </TodoList>
+          )}
+         </TodoContext.Consumer>
       
-        <CreateTodoButton
-         id='button' 
-         todos={todos}
-         setTodos={setTodos}
-         />
+        <CreateTodoButton />
       
         </React.Fragment>
         );

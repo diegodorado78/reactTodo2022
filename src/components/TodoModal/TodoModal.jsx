12b8633f85befa99380ca4d2fa671186d947e299
diego dorado <diegodorado78@gmail.com>
2022-07-19 19:16:17 -0500

    ready on mac

--- src/components/TodoModal/TodoModal.jsx
+++ src/components/TodoModal/TodoModal.jsx
@@ -1,17 +1,13 @@
-import ReactDOM from 'react-dom'
-import './TodoModal.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./TodoModal.css";
 
-    
-
-function TodoModal({children}) {// al llamar al modal le enviamos un hijo para que lo teletransporte y renderice
-    return ReactDOM.createPortal(
-        <div className='modal__background'>
-        {children}
-        </div>
-        ,
-        document.getElementById('modal')
-
-    )
+function TodoModal({ children }) {
+  // al llamar al modal le enviamos un hijo para que lo teletransporte y renderice
+  return ReactDOM.createPortal(
+    <div className="modal__background">{children}</div>,
+    document.getElementById("modal")
+  );
 }
 
-export {TodoModal};
+export { TodoModal };
